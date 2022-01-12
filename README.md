84248c6912fdafd3cb6539449c6851f54a6b176e
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-12 16:43:01 +0200

    add

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cella Beauty Center
+
+---
+
+
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Home from './home';
+import React from 'react';
+const index = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default index
+
