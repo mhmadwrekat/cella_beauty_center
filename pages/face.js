1701bd42a172e041a864255d630558d780a37b52
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-17 05:43:49 +0200

    test product

--- pages/face.js
+++ pages/face.js
@@ -77,6 +77,108 @@ const face = () => {
             </div>
           </section>
         </Slide>
+
+
+
+
+
+
+
+
+
+
+        <p className='pt-10 text-4xl font-semibold text-center sm:text-5xl text-sky-900'>منتجات برود افريكا</p>
+        <section className='grid grid-cols-2 justify-items-center ... gap-10 py-10 mx-5 sm:mx-0 sm:grid-cols-4 sm:gap-5'>
+
+
+
+
+
+        <section>
+            <div class="w-32 sm:w-60">
+                <div class="relative overflow-hidden shadow-2xl cursor-pointer rounded-xl">
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+                  <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">
+                  <p className='float-left py-1 text-sm font-semibold text-gray-500'>15JD</p>
+                      الشعر</h4>
+                </div>
+              <p className='px-4 pt-3 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="w-32 sm:w-60">
+                <div class="relative overflow-hidden shadow-2xl cursor-pointer rounded-xl">
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+                  <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">
+                  <p className='float-left py-1 text-xs font-semibold text-gray-500'>15JD</p>
+                      الشعر</h4>
+                </div>
+              <p className='px-4 pt-3 font-mono text-sm font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="w-32 sm:w-60">
+                <div class="relative overflow-hidden shadow-2xl cursor-pointer rounded-xl">
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+                  <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">
+                  <p className='float-left py-1 text-sm font-semibold text-gray-500'>15JD</p>
+                      الشعر</h4>
+                </div>
+              <p className='px-4 pt-3 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="w-32 sm:w-60">
+                <div class="relative overflow-hidden shadow-2xl cursor-pointer rounded-xl">
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+                  <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">
+                  <p className='float-left py-1 text-sm font-semibold text-gray-500'>15JD</p>
+                      الشعر</h4>
+                </div>
+              <p className='px-4 pt-3 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+
+
+
+
+
+
+
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <Footer />
       </section>
 
