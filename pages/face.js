25feb60905976372555b0dfec2b57de1f301345f
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-17 12:18:26 +0200

    UPDATE

--- pages/face.js
+++ pages/face.js
@@ -95,9 +95,9 @@ const face = () => {
                 <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
                   <img class="object-cover w-full sm:h-48 h-28"
                   src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
-<span class=" sm:px-2 sm:py-1 py-0 px-1 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
 <a href='https://wa.me/+962782950960'>
-<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">🛍️ 🛒</span>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">🛍️</span>
 </a>
                 </div>
                 <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
@@ -107,15 +107,14 @@ const face = () => {
           </section>
           <section>
             <div class="w-36 sm:w-60">
-                <div class="relative overflow-hidden rounded-3xl shadow-2xl">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
                 <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
                   <img class="object-cover w-full sm:h-48 h-28"
                   src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
-                  <p className=' py-0.5 pl-2'>
-                  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-1 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
-<a className='float-right' href='https://wa.me/+962782950960'>
-  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-2 text-xs font-bold leading-none text-white bg-green-600 rounded-full">اطلبه الان</span>
-</a></p>
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">🛒</span>
+</a>
                 </div>
                 <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
                 خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
@@ -124,32 +123,32 @@ const face = () => {
           </section>
           <section>
             <div class="w-36 sm:w-60">
-                <div class="relative overflow-hidden rounded-3xl shadow-2xl">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
                 <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
                   <img class="object-cover w-full sm:h-48 h-28"
                   src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
-<p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">🛍️🛒</span>
+</a>
+                </div>
+                <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
                 خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
               </p>
-                </div>
-                <p className=' py-0.5 pl-2'>
-                  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-1 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
-<a href='https://wa.me/+962782950960'>
-  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-2 text-xs font-bold leading-none text-white bg-green-600 rounded-full">اطلبه الان</span>
-</a>                  </p>
             </div>
           </section>
                   <section>
             <div class="w-36 sm:w-60">
-                <div class="relative overflow-hidden rounded-3xl shadow-2xl">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
                 <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
                   <img class="object-cover w-full sm:h-48 h-28"
                   src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
-                  <p className='text-center py-0.5 pl-2'>
-                  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-1 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
 <a href='https://wa.me/+962782950960'>
-  <span class="mr-1 sm:px-2 sm:py-1 py-0 px-2 text-xs font-bold leading-none text-white bg-green-600 rounded-full">اطلبه الان</span>
-</a>                  </p>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">
+<img src='https://cdn-icons-png.flaticon.com/512/6572/6572888.png' className='w-4 h-4'></img>
+</span>
+</a>
                 </div>
                 <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
                 خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
@@ -157,6 +156,87 @@ const face = () => {
             </div>
           </section>
 
+
+
+
+
+          <section>
+            <div class="w-36 sm:w-60">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
+                <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">
+<img src='https://cdn-icons-png.flaticon.com/512/3081/3081559.png' className='w-4 h-4'></img>
+</span>
+</a>
+                </div>
+                <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+
+
+
+          <section>
+            <div class="w-36 sm:w-60">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
+                <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">
+<img src='https://cdn-icons-png.flaticon.com/512/1170/1170576.png' className='w-4 h-4'></img>
+</span>
+</a>
+                </div>
+                <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+
+
+          <section>
+            <div class="w-36 sm:w-60">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
+                <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">
+<img src='https://cdn-icons-png.flaticon.com/512/3523/3523887.png' className='w-4 h-4'></img>
+</span>
+</a>
+                </div>
+                <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="w-36 sm:w-60">
+                <div class="relative overflow-hidden rounded-xl shadow-2xl">
+                <h4 class="my-1 text-sm font-semibold tracking-tight text-center sm:text-2xl text-sky-900">الشعر</h4>
+                  <img class="object-cover w-full sm:h-48 h-28"
+                  src="https://pbs.twimg.com/media/E3BF7PZXIAcBLGT?format=jpg&name=4096x4096" alt="Face" />
+<span class=" sm:px-2 sm:py-1 py-0 px-1.5 text-xs font-bold leading-none text-white bg-green-600 rounded-full">15JD</span>
+<a href='https://wa.me/+962782950960'>
+<span class="py-1 sm:px-2 sm:py-1 px-1 text-xs font-bold leading-none float-right rounded-full">
+<img src='https://cdn-icons.flaticon.com/png/512/3145/premium/3145827.png?token=exp=1642415465~hmac=4185bd4bbde4e492dd25aed100f3687c' className='w-4 h-4'></img>
+</span>
+</a>
+                </div>
+                <p className='px-4 py-2 font-mono text-xs font-bold text-center sm:text-medium text-sky-900'>
+                خدماتنا هي وسيلة رائعة للتراجع عن صخب الحياة اليومية في عمان
+              </p>
+            </div>
+          </section>
 
 
 
