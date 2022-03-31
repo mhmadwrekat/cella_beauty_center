ac79bcf48626494cc45daaea8a1574e4dbc365de
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-03-31 21:13:13 +0300

    Update

--- components/Hero.js
+++ components/Hero.js
@@ -4,15 +4,15 @@ const Hero = () => {
     return (
         <>
             <div className='pt-16'></div>
-            <div className="bg-center bg-cover h-screen bg-[url('https://img5.goodfon.com/wallpaper/nbig/b/aa/model-litso-makiiazh-1.jpg')]">
+            <div className="bg-right bg-cover h-screen bg-[url('https://img4.goodfon.com/wallpaper/nbig/c/6e/devushka-model-makiiazh-resnitsy-guby-pomada-dlinnye-volosy.jpg')]">
                 <div className='hidden sm:flex'>
-                    <h4 translate='no' className='absolute float-right text-3xl font-semibold text-right text-sky-900 w-96 top-80 left-32'>
+                    <h4 translate='no' className='absolute float-right text-3xl font-semibold text-right text-sky-900 w-96 top-80 left-28'>
                         مركز سيلا للتجميل هو مركز تجميل على احدث طراز,
                         متخصصون في تصفيف الشعر والمكياج للعرائس, احدث صيحات الالوان والعناية بالأظافر والسكر وعلاجات الوجه والمزيد!!
                     </h4>
                 </div>
             </div>
-            <section className='absolute w-full text-center bg-white opacity-70 sm:hidden top-72'>
+            <section className='absolute w-full text-center bg-white opacity-70 sm:hidden top-80'>
                 <section className='relative'>
                     <div class="relative py-5 sm:hidden overflow-hidden rounded-lg shadow-2xl cursor-pointer">
                         <h4 translate='no' class="mb-3 text-xl font-semibold text-center tracking-tight text-sky-900"><h4 className='px-5 text-lg font-semibold text-center text-sky-900'>
